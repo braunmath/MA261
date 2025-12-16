f58d67ed1f01a20d555c256b160d013b6f194404
Benjamin Braun <38571546+braunmath@users.noreply.github.com>
2025-12-16 17:25:58 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -70,7 +70,430 @@ var ptx_lunr_docs = [
   "type": "Chapter",
   "number": "8",
   "title": "Problems and Essays",
-  "body": " Problems and Essays    "
+  "body": " Problems and Essays   P1      P2      Essay 1      P3      P4      P5      Essay 2      P6      P7      P8      P9      P10      P11      P12      P13      P14      P15      P16      P17      Essay 3  Read the following blog post by Keith Weber, a professor of math education at Rutgers University who studies how undergraduate students understand proofs in mathematics: After you read this article, complete the following essay.   This will be an essay of length at least 500 words, which is equivalent to at least 2 typed pages with 1 inch margins, 12 point Times New Roman font, double spaced. (You can write a longer essay if needed.)    Your essay should respond to the following three questions.   The article describes four expectations that professors have for students, but which are usually mis-communicated. For each of these four expectations , do you respond more like the students in their surveys, or more like the professors? Why?    The discussion at the end of the article has some recommendations for faculty in their courses. Which of these recommendations do you think you would find most helpful for your learning, and why?    What is one thing you might change about your approach to your math courses after reading this article? Why is this the thing you would choose to change?      Your grade is based on completion only (in other words, if you complete the assignment and it meets the length requirement and responds appropriately to the prompt, then you get full credit).    P18      P19      P20      P21      P22      P23      Essay 4  Watch the following YouTube video from Quanta Magazine (17 minutes, published January 2021): After you watch this video, complete the following essay.   This will be an essay of length 500 words, which is equivalent to 2 typed pages with 1 inch margins, 12 point Times New Roman font, double spaced.    Write a critical review of this video. Imagine that you are writing your review for a journal for undergraduates in mathematics and the sciences, so your primary audience is undergraduate math\/cs\/engineering\/etc majors and minors.    Like any critic, you will respond positively to some things and negatively to others. Unlike many critics, you need to justify your opinions and provide detailed explanations for your claims.    You should consider the following questions:     Does the video effectively communicate the mathematics it discusses? Why or why not? Justify your claims with specific details about the video.    We have discussed multiple approaches\/themes to prime numbers in this course. Which of these approaches\/themes to prime numbers are used in this video? Be detailed, give specific examples from the video!    Is the mathematics clearly explained? If yes, what did they do especially well? If no, what made it unclear? Again, justify your claims with specific examples from the video.    Your grade is based on completion only (in other words, if you complete the assignment and it meets the length requirement and responds appropriately to the prompt, then you get full credit).    P24      P25      P26      Essay 5      P27      P28      P29      P30      P31      P32      Essay 6      P33      P34      P35      P36      P37      P38      P39      P40      End-of-Class Reflection  Type a 1000 word essay in response to the following prompt. This is the equivalent of 4 pages in 12 point Times New Roman font, double-spaced.   What were six of the most important discoveries or realizations you made in this class? In other words, what are you taking away from this class that you think might stick with you over time and\/or influence you in the future? What have you experienced that might have a long-term effect on you intellectually or personally? These can include things you had not realized about mathematics or society, specific homework problems or theorems, etc. These can be things that made sense to you, or topics where you were confused, points that you agreed\/disagreed with in the readings or class discussions, issues that arose while working on your assignments, etc.    Explain why these six discoveries or realizations are important to you.    You should include a combination of observations about both mathematics and your habits, practices, and beliefs about mathematics.   This assignment grade is based only on completion (i.e. if you turn it in, it meets the length requirement, and it appropriately responds to the prompt above, then you get full credit).   "
+},
+{
+  "id": "problems-2",
+  "level": "2",
+  "url": "problems.html#problems-2",
+  "type": "Problem",
+  "number": "8.0.1",
+  "title": "P1.",
+  "body": " P1    "
+},
+{
+  "id": "problems-3",
+  "level": "2",
+  "url": "problems.html#problems-3",
+  "type": "Problem",
+  "number": "8.0.2",
+  "title": "P2.",
+  "body": " P2    "
+},
+{
+  "id": "problems-4",
+  "level": "2",
+  "url": "problems.html#problems-4",
+  "type": "Problem",
+  "number": "8.0.3",
+  "title": "Essay 1.",
+  "body": " Essay 1    "
+},
+{
+  "id": "problems-5",
+  "level": "2",
+  "url": "problems.html#problems-5",
+  "type": "Problem",
+  "number": "8.0.4",
+  "title": "P3.",
+  "body": " P3    "
+},
+{
+  "id": "problems-6",
+  "level": "2",
+  "url": "problems.html#problems-6",
+  "type": "Problem",
+  "number": "8.0.5",
+  "title": "P4.",
+  "body": " P4    "
+},
+{
+  "id": "problems-7",
+  "level": "2",
+  "url": "problems.html#problems-7",
+  "type": "Problem",
+  "number": "8.0.6",
+  "title": "P5.",
+  "body": " P5    "
+},
+{
+  "id": "problems-8",
+  "level": "2",
+  "url": "problems.html#problems-8",
+  "type": "Problem",
+  "number": "8.0.7",
+  "title": "Essay 2.",
+  "body": " Essay 2    "
+},
+{
+  "id": "problems-9",
+  "level": "2",
+  "url": "problems.html#problems-9",
+  "type": "Problem",
+  "number": "8.0.8",
+  "title": "P6.",
+  "body": " P6    "
+},
+{
+  "id": "problems-10",
+  "level": "2",
+  "url": "problems.html#problems-10",
+  "type": "Problem",
+  "number": "8.0.9",
+  "title": "P7.",
+  "body": " P7    "
+},
+{
+  "id": "problems-11",
+  "level": "2",
+  "url": "problems.html#problems-11",
+  "type": "Problem",
+  "number": "8.0.10",
+  "title": "P8.",
+  "body": " P8    "
+},
+{
+  "id": "problems-12",
+  "level": "2",
+  "url": "problems.html#problems-12",
+  "type": "Problem",
+  "number": "8.0.11",
+  "title": "P9.",
+  "body": " P9    "
+},
+{
+  "id": "problems-13",
+  "level": "2",
+  "url": "problems.html#problems-13",
+  "type": "Problem",
+  "number": "8.0.12",
+  "title": "P10.",
+  "body": " P10    "
+},
+{
+  "id": "problems-14",
+  "level": "2",
+  "url": "problems.html#problems-14",
+  "type": "Problem",
+  "number": "8.0.13",
+  "title": "P11.",
+  "body": " P11    "
+},
+{
+  "id": "problems-15",
+  "level": "2",
+  "url": "problems.html#problems-15",
+  "type": "Problem",
+  "number": "8.0.14",
+  "title": "P12.",
+  "body": " P12    "
+},
+{
+  "id": "problems-16",
+  "level": "2",
+  "url": "problems.html#problems-16",
+  "type": "Problem",
+  "number": "8.0.15",
+  "title": "P13.",
+  "body": " P13    "
+},
+{
+  "id": "problems-17",
+  "level": "2",
+  "url": "problems.html#problems-17",
+  "type": "Problem",
+  "number": "8.0.16",
+  "title": "P14.",
+  "body": " P14    "
+},
+{
+  "id": "problems-18",
+  "level": "2",
+  "url": "problems.html#problems-18",
+  "type": "Problem",
+  "number": "8.0.17",
+  "title": "P15.",
+  "body": " P15    "
+},
+{
+  "id": "problems-19",
+  "level": "2",
+  "url": "problems.html#problems-19",
+  "type": "Problem",
+  "number": "8.0.18",
+  "title": "P16.",
+  "body": " P16    "
+},
+{
+  "id": "problems-20",
+  "level": "2",
+  "url": "problems.html#problems-20",
+  "type": "Problem",
+  "number": "8.0.19",
+  "title": "P17.",
+  "body": " P17    "
+},
+{
+  "id": "problems-21",
+  "level": "2",
+  "url": "problems.html#problems-21",
+  "type": "Problem",
+  "number": "8.0.20",
+  "title": "Essay 3.",
+  "body": " Essay 3  Read the following blog post by Keith Weber, a professor of math education at Rutgers University who studies how undergraduate students understand proofs in mathematics: After you read this article, complete the following essay.   This will be an essay of length at least 500 words, which is equivalent to at least 2 typed pages with 1 inch margins, 12 point Times New Roman font, double spaced. (You can write a longer essay if needed.)    Your essay should respond to the following three questions.   The article describes four expectations that professors have for students, but which are usually mis-communicated. For each of these four expectations , do you respond more like the students in their surveys, or more like the professors? Why?    The discussion at the end of the article has some recommendations for faculty in their courses. Which of these recommendations do you think you would find most helpful for your learning, and why?    What is one thing you might change about your approach to your math courses after reading this article? Why is this the thing you would choose to change?      Your grade is based on completion only (in other words, if you complete the assignment and it meets the length requirement and responds appropriately to the prompt, then you get full credit).  "
+},
+{
+  "id": "problems-22",
+  "level": "2",
+  "url": "problems.html#problems-22",
+  "type": "Problem",
+  "number": "8.0.21",
+  "title": "P18.",
+  "body": " P18    "
+},
+{
+  "id": "problems-23",
+  "level": "2",
+  "url": "problems.html#problems-23",
+  "type": "Problem",
+  "number": "8.0.22",
+  "title": "P19.",
+  "body": " P19    "
+},
+{
+  "id": "problems-24",
+  "level": "2",
+  "url": "problems.html#problems-24",
+  "type": "Problem",
+  "number": "8.0.23",
+  "title": "P20.",
+  "body": " P20    "
+},
+{
+  "id": "problems-25",
+  "level": "2",
+  "url": "problems.html#problems-25",
+  "type": "Problem",
+  "number": "8.0.24",
+  "title": "P21.",
+  "body": " P21    "
+},
+{
+  "id": "problems-26",
+  "level": "2",
+  "url": "problems.html#problems-26",
+  "type": "Problem",
+  "number": "8.0.25",
+  "title": "P22.",
+  "body": " P22    "
+},
+{
+  "id": "problems-27",
+  "level": "2",
+  "url": "problems.html#problems-27",
+  "type": "Problem",
+  "number": "8.0.26",
+  "title": "P23.",
+  "body": " P23    "
+},
+{
+  "id": "problems-28",
+  "level": "2",
+  "url": "problems.html#problems-28",
+  "type": "Problem",
+  "number": "8.0.27",
+  "title": "Essay 4.",
+  "body": " Essay 4  Watch the following YouTube video from Quanta Magazine (17 minutes, published January 2021): After you watch this video, complete the following essay.   This will be an essay of length 500 words, which is equivalent to 2 typed pages with 1 inch margins, 12 point Times New Roman font, double spaced.    Write a critical review of this video. Imagine that you are writing your review for a journal for undergraduates in mathematics and the sciences, so your primary audience is undergraduate math\/cs\/engineering\/etc majors and minors.    Like any critic, you will respond positively to some things and negatively to others. Unlike many critics, you need to justify your opinions and provide detailed explanations for your claims.    You should consider the following questions:     Does the video effectively communicate the mathematics it discusses? Why or why not? Justify your claims with specific details about the video.    We have discussed multiple approaches\/themes to prime numbers in this course. Which of these approaches\/themes to prime numbers are used in this video? Be detailed, give specific examples from the video!    Is the mathematics clearly explained? If yes, what did they do especially well? If no, what made it unclear? Again, justify your claims with specific examples from the video.    Your grade is based on completion only (in other words, if you complete the assignment and it meets the length requirement and responds appropriately to the prompt, then you get full credit).  "
+},
+{
+  "id": "problems-29",
+  "level": "2",
+  "url": "problems.html#problems-29",
+  "type": "Problem",
+  "number": "8.0.28",
+  "title": "P24.",
+  "body": " P24    "
+},
+{
+  "id": "problems-30",
+  "level": "2",
+  "url": "problems.html#problems-30",
+  "type": "Problem",
+  "number": "8.0.29",
+  "title": "P25.",
+  "body": " P25    "
+},
+{
+  "id": "problems-31",
+  "level": "2",
+  "url": "problems.html#problems-31",
+  "type": "Problem",
+  "number": "8.0.30",
+  "title": "P26.",
+  "body": " P26    "
+},
+{
+  "id": "problems-32",
+  "level": "2",
+  "url": "problems.html#problems-32",
+  "type": "Problem",
+  "number": "8.0.31",
+  "title": "Essay 5.",
+  "body": " Essay 5    "
+},
+{
+  "id": "problems-33",
+  "level": "2",
+  "url": "problems.html#problems-33",
+  "type": "Problem",
+  "number": "8.0.32",
+  "title": "P27.",
+  "body": " P27    "
+},
+{
+  "id": "problems-34",
+  "level": "2",
+  "url": "problems.html#problems-34",
+  "type": "Problem",
+  "number": "8.0.33",
+  "title": "P28.",
+  "body": " P28    "
+},
+{
+  "id": "problems-35",
+  "level": "2",
+  "url": "problems.html#problems-35",
+  "type": "Problem",
+  "number": "8.0.34",
+  "title": "P29.",
+  "body": " P29    "
+},
+{
+  "id": "problems-36",
+  "level": "2",
+  "url": "problems.html#problems-36",
+  "type": "Problem",
+  "number": "8.0.35",
+  "title": "P30.",
+  "body": " P30    "
+},
+{
+  "id": "problems-37",
+  "level": "2",
+  "url": "problems.html#problems-37",
+  "type": "Problem",
+  "number": "8.0.36",
+  "title": "P31.",
+  "body": " P31    "
+},
+{
+  "id": "problems-38",
+  "level": "2",
+  "url": "problems.html#problems-38",
+  "type": "Problem",
+  "number": "8.0.37",
+  "title": "P32.",
+  "body": " P32    "
+},
+{
+  "id": "problems-39",
+  "level": "2",
+  "url": "problems.html#problems-39",
+  "type": "Problem",
+  "number": "8.0.38",
+  "title": "Essay 6.",
+  "body": " Essay 6    "
+},
+{
+  "id": "problems-40",
+  "level": "2",
+  "url": "problems.html#problems-40",
+  "type": "Problem",
+  "number": "8.0.39",
+  "title": "P33.",
+  "body": " P33    "
+},
+{
+  "id": "problems-41",
+  "level": "2",
+  "url": "problems.html#problems-41",
+  "type": "Problem",
+  "number": "8.0.40",
+  "title": "P34.",
+  "body": " P34    "
+},
+{
+  "id": "problems-42",
+  "level": "2",
+  "url": "problems.html#problems-42",
+  "type": "Problem",
+  "number": "8.0.41",
+  "title": "P35.",
+  "body": " P35    "
+},
+{
+  "id": "problems-43",
+  "level": "2",
+  "url": "problems.html#problems-43",
+  "type": "Problem",
+  "number": "8.0.42",
+  "title": "P36.",
+  "body": " P36    "
+},
+{
+  "id": "problems-44",
+  "level": "2",
+  "url": "problems.html#problems-44",
+  "type": "Problem",
+  "number": "8.0.43",
+  "title": "P37.",
+  "body": " P37    "
+},
+{
+  "id": "problems-45",
+  "level": "2",
+  "url": "problems.html#problems-45",
+  "type": "Problem",
+  "number": "8.0.44",
+  "title": "P38.",
+  "body": " P38    "
+},
+{
+  "id": "problems-46",
+  "level": "2",
+  "url": "problems.html#problems-46",
+  "type": "Problem",
+  "number": "8.0.45",
+  "title": "P39.",
+  "body": " P39    "
+},
+{
+  "id": "problems-47",
+  "level": "2",
+  "url": "problems.html#problems-47",
+  "type": "Problem",
+  "number": "8.0.46",
+  "title": "P40.",
+  "body": " P40    "
+},
+{
+  "id": "problems-48",
+  "level": "2",
+  "url": "problems.html#problems-48",
+  "type": "Problem",
+  "number": "8.0.47",
+  "title": "End-of-Class Reflection.",
+  "body": " End-of-Class Reflection  Type a 1000 word essay in response to the following prompt. This is the equivalent of 4 pages in 12 point Times New Roman font, double-spaced.   What were six of the most important discoveries or realizations you made in this class? In other words, what are you taking away from this class that you think might stick with you over time and\/or influence you in the future? What have you experienced that might have a long-term effect on you intellectually or personally? These can include things you had not realized about mathematics or society, specific homework problems or theorems, etc. These can be things that made sense to you, or topics where you were confused, points that you agreed\/disagreed with in the readings or class discussions, issues that arose while working on your assignments, etc.    Explain why these six discoveries or realizations are important to you.    You should include a combination of observations about both mathematics and your habits, practices, and beliefs about mathematics.   This assignment grade is based only on completion (i.e. if you turn it in, it meets the length requirement, and it appropriately responds to the prompt above, then you get full credit).  "
 }
 ]
 
